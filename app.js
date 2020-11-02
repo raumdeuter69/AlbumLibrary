2efdd788abb8b875b981a25fb5a9403b4cabc4dd
raumdeuter69 <53443414+raumdeuter69@users.noreply.github.com>
2020-11-03 03:09:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  const list = document.querySelector('#book-list ul');
+  const list = document.querySelector('#album-list ul');
   const forms = document.forms;
 
-  // delete books
+  // delete albums
   list.addEventListener('click', (e) => {
     if(e.target.className == 'delete'){
       const li = e.target.parentElement;
@@ -11,32 +11,32 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   });
 
-  // add books
-  const addForm = forms['add-book'];
+  // add albums
+  const addForm = forms['add-album'];
   addForm.addEventListener('submit', function(e){
     e.preventDefault();
 
     // create elements
     const value = addForm.querySelector('input[type="text"]').value;
     const li = document.createElement('li');
-    const bookName = document.createElement('span');
+    const albumName = document.createElement('span');
     const deleteBtn = document.createElement('span');
 
     // add text content
-    bookName.textContent = value;
+    albumName.textContent = value;
     deleteBtn.textContent = 'delete';
 
     // add classes
-    bookName.classList.add('name');
+    albumName.classList.add('name');
     deleteBtn.classList.add('delete');
 
     // append to DOM
-    li.appendChild(bookName);
+    li.appendChild(albumName);
     li.appendChild(deleteBtn);
     list.appendChild(li);
   });
 
-  // hide books
+  // hide albums
   const hideBox = document.querySelector('#hide');
   hideBox.addEventListener('change', function(e){
     if(hideBox.checked){
@@ -46,17 +46,17 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   });
 
-  // filter books
-  const searchBar = forms['search-books'].querySelector('input');
+  // filter albums
+  const searchBar = forms['search-albums'].querySelector('input');
   searchBar.addEventListener('keyup', (e) => {
     const term = e.target.value.toLowerCase();
-    const books = list.getElementsByTagName('li');
-    Array.from(books).forEach((book) => {
-      const title = book.firstElementChild.textContent;
+    const albums = list.getElementsByTagName('li');
+    Array.from(albums).forEach((album) => {
+      const title = album.firstElementChild.textContent;
       if(title.toLowerCase().indexOf(e.target.value) != -1){
-        book.style.display = 'block';
+        album.style.display = 'block';
       } else {
-        book.style.display = 'none';
+        album.style.display = 'none';
       }
     });
   });
